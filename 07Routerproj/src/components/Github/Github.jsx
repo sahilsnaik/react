85b410466e7d27aaa741e287ad5a036330883d84
unknown <sn220222@gmail.com>
2024-03-20 20:17:25 +0530

    add

---
+++ 07Routerproj/src/components/Github/Github.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect,useState } from 'react'
+import { useLoaderData } from 'react-router-dom';
+function Github() {
+    
+        const data = useLoaderData()
+    
+    // const [data,setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/sankethsk')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // },[])
+  return (
+    <div className='text-center m-4  bg-gray-600
+    p-4 text-white text-3xl'>Github Followers:{data.followers} 
+    <img src={data.avatar_url} alt="git picture" 
+    width = {300} /></div>
+  )
+}
+
+export default Github;
+
+export const githubInfoLoader = async() => {
+    const response = await fetch('https://api.github.com/users/sankethsk')
+    
+    return response.json()
+}
